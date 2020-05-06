d9a1b34d216a4614934c08f7aab03d89665204f2
Lane876 <milenkovicmilan10@gmail.com>
2020-05-06 20:48:41 +0200

    accounts page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,100 @@
 import React from "react";
 import { useLoginPageStyles } from "../styles";
+import SEO from "../components/shared/Seo";
+import {
+  Card,
+  CardHeader,
+  TextField,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+import FaceBookIconBlue from "../images/facebook-icon-blue.svg";
+import FaceBookIconWhite from "../images/facebook-icon-white.png";
 
 function LoginPage() {
-  useLoginPageStyles();
+  const classes = useLoginPageStyles();
 
-  return <div>LoginPage</div>;
+  return (
+    <>
+      <SEO title="Login" />
+      <section className={classes.section}>
+        <article>
+          <Card className={classes.card}>
+            <CardHeader className={classes.cardHeader} />
+            <from>
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Username"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="username"
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Password"
+                type="password"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="current-password"
+              />
+              <Button
+                variant="contained"
+                fullWidth
+                className={classes.button}
+                color="primary"
+                type="submit"
+              >
+                Log In
+              </Button>
+              <dvi className={classes.orContainer}>
+                <div className={classes.orLine} />
+                <div>
+                  <Typography varinat="body2" color="textSecondary">
+                    OR
+                  </Typography>
+                </div>
+                <div className={classes.orLine} />
+              </dvi>
+              <LoginWithFacebook color="secondary" iconColor="blue" />
+              <Button color="secondary" fullWidth>
+                <Typography variant="caption">Forgot password?</Typography>
+              </Button>
+            </from>
+          </Card>
+          <Card className={classes.signUpCard}>
+            <Typography align="right" variant="body2">
+              Don't have an account?
+            </Typography>
+            <Link to="/accounts/emailsignup">
+              <Button color="primary" className={classes.button}>
+                Sign Up
+              </Button>
+            </Link>
+          </Card>
+        </article>
+      </section>
+    </>
+  );
+}
+
+export function LoginWithFacebook({ color, iconColor, variant }) {
+  const classes = useLoginPageStyles();
+  const facebookIcon =
+    iconColor === "blue" ? FaceBookIconBlue : FaceBookIconWhite;
+
+  return (
+    <Button fullWidth color={color} variant={variant}>
+      <img
+        src={facebookIcon}
+        alt="facebook icon"
+        className={classes.facebookIcon}
+      />
+      Log In with Facebook
+    </Button>
+  );
 }
 
 export default LoginPage;
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,10 +1,104 @@
 import React from "react";
 import { useSignUpPageStyles } from "../styles";
+import SEO from "../components/shared/Seo";
+import {
+  Card,
+  CardHeader,
+  TextField,
+  Button,
+  Typography,
+} from "@material-ui/core";
+import { Link } from "react-router-dom";
+import { LoginWithFacebook } from "./login";
 
 function SignUpPage() {
-  useSignUpPageStyles();
+  const classes = useSignUpPageStyles();
 
-  return <div>SignUpPage</div>;
+  return (
+    <>
+      <SEO title="Sign up" />
+      <section className={classes.section}>
+        <article>
+          <Card className={classes.card}>
+            <CardHeader className={classes.cardHeader} />
+            <Typography className={classes.cardHeaderSubHeader}>
+              Sing up to see photos and videos from your friends.
+            </Typography>
+            <LoginWithFacebook
+              color="primary"
+              iconColor="white"
+              variant="contained"
+            />
+            <dvi className={classes.orContainer}>
+              <div className={classes.orLine} />
+              <div>
+                <Typography varinat="body2" color="textSecondary">
+                  OR
+                </Typography>
+              </div>
+              <div className={classes.orLine} />
+            </dvi>
+            <from>
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Email"
+                type="email"
+                margin="dense"
+                className={classes.textField}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Full Name"
+                margin="dense"
+                className={classes.textField}
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Username"
+                margin="dense"
+                className={classes.textField}
+                autoComplete="username"
+              />
+              <TextField
+                fullWidth
+                variant="filled"
+                label="Password"
+                margin="dense"
+                type="password"
+                className={classes.textField}
+                autoComplete="new-password"
+              />
+              <Button
+                variant="contained"
+                fullWidth
+                className={classes.button}
+                color="primary"
+                type="submit"
+              >
+                Sign Up
+              </Button>
+              <Button color="secondary" fullWidth>
+                <Typography variant="caption">Forgot password?</Typography>
+              </Button>
+            </from>
+          </Card>
+          <Card className={classes.loginCard}>
+            <Typography align="right" variant="body2">
+              Have an account?
+            </Typography>
+            <Link to="/accounts/login">
+              <Button color="primary" className={classes.loginButton}>
+                Log in
+              </Button>
+            </Link>
+          </Card>
+        </article>
+      </section>
+    </>
+  );
 }
 
 export default SignUpPage;
