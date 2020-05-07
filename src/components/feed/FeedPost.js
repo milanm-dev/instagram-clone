30a645c2c4da4c4862c7a05cc34ec25e26bcd80f
Lane876 <milenkovicmilan10@gmail.com>
2020-05-07 09:21:03 +0200

    FeedPost

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -1,10 +1,124 @@
 import React from "react";
 import { useFeedPostStyles } from "../../styles";
+import UserCard from "../shared/UserCard";
+import { MoreIcon, CommentIcon, ShareIcon } from "../../icons";
+import { Link } from "react-router-dom";
+import { Typography, Button, Hidden, Divider } from "@material-ui/core";
+import HTMLEllipsis from "react-lines-ellipsis/lib/html";
 
-function FeedPost() {
-  useFeedPostStyles();
+function FeedPost({ post }) {
+  const classes = useFeedPostStyles();
+  const { id, media, likes, user, caption, comments } = post;
+  const [showCaption, setCaption] = React.useState(false);
 
-  return <div>FeedPost</div>;
+  return (
+    <>
+      <article className={classes.article}>
+        {/* FeedPost Header */}
+        <div className={classes.postHeader}>
+          <UserCard />
+          <MoreIcon className={classes.moreIcon} />
+        </div>
+        {/* Feed Post Image */}
+        <div>
+          <img src={media} alt="post media" className={classes.image} />
+        </div>
+        {/* Feed post buttons */}
+        <div className={classes.postButtonsWrapper}>
+          <div className={classes.postButtons}>
+            <LikeButton />
+            <Link to={`/p/${id}`}>
+              <CommentIcon />
+            </Link>
+            <ShareIcon />
+            <SaveButton />
+          </div>
+          <Typography className={classes.likes} variant="subtitle2">
+            <span>{likes === 1 ? "1 like" : `${likes} likes`}</span>
+          </Typography>
+          <div className={showCaption ? classes.expanded : classes.collapsed}>
+            <Link to={`/${user.username}`}>
+              <Typography
+                variant="subtitle2"
+                component="span"
+                className={classes.username}
+              >
+                {user.username}
+              </Typography>
+            </Link>
+            {showCaption ? (
+              <Typography
+                variant="body2"
+                component="span"
+                dangerouslySetInnerHTML={{ __html: caption }}
+              ></Typography>
+            ) : (
+              <div className={classes.captionWrapper}>
+                <HTMLEllipsis
+                  unsafeHTML={caption}
+                  className={classes.caption}
+                  maxLine="0"
+                  ellipsis="..."
+                  basedOn="letters"
+                />
+
+                <Button
+                  className={classes.moreButton}
+                  onClick={() => setCaption(true)}
+                >
+                  more
+                </Button>
+              </div>
+            )}
+          </div>
+          <Link to={`/p/${id}`}>
+            <Typography
+              className={classes.commentsLink}
+              variant="body2"
+              component="div"
+            >
+              View all {comments.length} comments
+            </Typography>
+          </Link>
+          {comments.map((comment) => (
+            <div key={comment.id}>
+              <Link to={`/${comment.user.username}`}>
+                <Typography
+                  variant="subtitle2"
+                  className={classes.commentUsername}
+                  component="span"
+                >
+                  {comment.user.username}
+                </Typography>{" "}
+                <Typography variant="body2" component="span">
+                  {comment.content}
+                </Typography>
+              </Link>
+            </div>
+          ))}
+          <Typography color="textSecondary" className={classes.datePosted}>
+            5 DAYS AGO
+          </Typography>
+        </div>
+        <Hidden xsDown>
+          <Divider />
+          <Comment />
+        </Hidden>
+      </article>
+    </>
+  );
+}
+
+function LikeButton() {
+  return <>LikeButton</>;
+}
+
+function SaveButton() {
+  return <>SaveButton</>;
+}
+
+function Comment() {
+  return <>Comment</>;
 }
 
 export default FeedPost;
