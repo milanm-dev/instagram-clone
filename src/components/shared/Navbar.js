1cdde1a1d1c678ea421cc2520e8d75c4048788a2
Lane876 <milenkovicmilan10@gmail.com>
2020-05-09 08:59:43 +0200

    navbar updated

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavbarStyles, WhiteTooltip } from "../../styles";
+import { useNavbarStyles, WhiteTooltip, RedTooltip } from "../../styles";
 import {
   AppBar,
   Hidden,
@@ -8,6 +8,7 @@ import {
   Grid,
   Fade,
   Typography,
+  Zoom,
 } from "@material-ui/core";
 import { Link, useHistory } from "react-router-dom";
 import logo from "../../images/logo.png";
@@ -22,20 +23,31 @@ import {
   HomeActiveIcon,
 } from "../../icons";
 import { defaultCurrentUser, getDefaultUser } from "../../data";
+import NotificationTooltip from "../notification/NotificationTooltip";
+import NotificationList from "../notification/NotificationList";
+import { useNProgress } from "@tanem/react-nprogress";
 
 function Navbar({ minimalNavbar }) {
   const classes = useNavbarStyles();
   const history = useHistory();
   const path = history.location.pathname;
+  const [isLoadingPage, setLoadingPage] = React.useState(true);
+
+  React.useEffect(() => {
+    setLoadingPage(false);
+  }, [path]);
 
   return (
-    <AppBar className={classes.appBar}>
-      <section className={classes.section}>
-        <Logo />
-        {!minimalNavbar && <Search />}
-        {!minimalNavbar && <Links path={path} />}
-      </section>
-    </AppBar>
+    <>
+      <Progress isAnimating={isLoadingPage} />
+      <AppBar className={classes.appBar}>
+        <section className={classes.section}>
+          <Logo />
+          {!minimalNavbar && <Search history={history} />}
+          {!minimalNavbar && <Links path={path} />}
+        </section>
+      </AppBar>
+    </>
   );
 }
 
@@ -53,7 +65,7 @@ function Logo() {
   );
 }
 
-function Search() {
+function Search({ history }) {
   const classes = useNavbarStyles();
   const [loading, setLoading] = React.useState(false);
   const [results, setResults] = React.useState([]);
@@ -81,7 +93,15 @@ function Search() {
           hasResult && (
             <Grid className={classes.resultContainer} container>
               {results.map((result) => (
-                <Grid key={result.id} item className={classes.reultLink}>
+                <Grid
+                  key={result.id}
+                  item
+                  className={classes.reultLink}
+                  onClick={() => {
+                    history.push(`/${result.username}`);
+                    handleClearInput();
+                  }}
+                >
                   <div className={classes.resultWrapper}>
                     <div className={classes.avatarWrapper}>
                       <Avatar src={result.profile_image} alt="user avatar" />
@@ -120,14 +140,30 @@ function Search() {
 
 function Links({ path }) {
   const classes = useNavbarStyles();
+  const [showTooltip, setTooltip] = React.useState(true);
   const [showList, setList] = React.useState(false);
+
+  React.useEffect(() => {
+    const timeout = setTimeout(handleHideTooltip, 3000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  function handleHideList() {
+    setList(false);
+  }
 
   function handleToggleList() {
     setList((prevState) => !prevState);
   }
+  function handleHideTooltip() {
+    setTooltip(false);
+  }
 
   return (
     <div className={classes.linksContainer}>
+      {showList && <NotificationList handleHideList={handleHideList} />}
       <div className={classes.linksWrapper}>
         <Hidden xsDown>
           <AddIcon />
@@ -136,9 +172,17 @@ function Links({ path }) {
         <Link to="/explore">
           {path === "/explore" ? <ExploreActiveIcon /> : <ExploreIcon />}
         </Link>
-        <div className={classes.notifications} onClick={handleToggleList}>
-          {showList ? <LikeActiveIcon /> : <LikeIcon />}
-        </div>
+        <RedTooltip
+          arrow
+          open={showTooltip}
+          onOpen={handleHideTooltip}
+          TransitionComponent={Zoom}
+          title={<NotificationTooltip />}
+        >
+          <div className={classes.notifications} onClick={handleToggleList}>
+            {showList ? <LikeActiveIcon /> : <LikeIcon />}
+          </div>
+        </RedTooltip>
         <Link to={`/${defaultCurrentUser.username}`}>
           <div
             className={
@@ -152,6 +196,33 @@ function Links({ path }) {
             className={classes.profileImage}
           />
         </Link>
+      </div>
+    </div>
+  );
+}
+
+function Progress({ isAnimating }) {
+  const classes = useNavbarStyles();
+  const { animationDuration, isFinished, progress } = useNProgress({
+    isAnimating,
+  });
+
+  return (
+    <div
+      className={classes.progressContainer}
+      style={{
+        opacity: isFinished ? 0 : 1,
+        transition: `opacity ${animationDuration}ms linear`,
+      }}
+    >
+      <div
+        className={classes.progressBar}
+        style={{
+          marginLeft: `${-1 * progress * 100}% `,
+          transition: `margin-left ${animationDuration}ms linear`,
+        }}
+      >
+        <div className={classes.progressBackground} />
       </div>
     </div>
   );
