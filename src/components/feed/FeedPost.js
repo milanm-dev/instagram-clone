9cb69224d6fdd42d767e76b309e33580c4456fae
Lane876 <milenkovicmilan10@gmail.com>
2020-05-09 17:16:41 +0200

    followSuggestions

--- src/components/feed/FeedPost.js
+++ src/components/feed/FeedPost.js
@@ -19,19 +19,32 @@ import {
   TextField,
 } from "@material-ui/core";
 import HTMLEllipsis from "react-lines-ellipsis/lib/html";
+import FollowSuggestion from "../shared/FollowSuggestions";
+import OptionsDialog from "../shared/OptionsDialog";
 
-function FeedPost({ post }) {
+function FeedPost({ post, index }) {
   const classes = useFeedPostStyles();
   const { id, media, likes, user, caption, comments } = post;
   const [showCaption, setCaption] = React.useState(false);
+  const [showOptionsDialog, setOptionsDialog] = React.useState(false);
+
+  const showFollowSuggestions = index === 1;
 
   return (
     <>
-      <article className={classes.article}>
+      <article
+        className={classes.article}
+        style={{
+          marginBottom: showFollowSuggestions && 30,
+        }}
+      >
         {/* FeedPost Header */}
         <div className={classes.postHeader}>
           <UserCard user={user} />
-          <MoreIcon className={classes.moreIcon} />
+          <MoreIcon
+            className={classes.moreIcon}
+            onClick={() => setOptionsDialog(true)}
+          />
         </div>
         {/* Feed Post Image */}
         <div>
@@ -119,6 +132,10 @@ function FeedPost({ post }) {
           <Comment />
         </Hidden>
       </article>
+      {showFollowSuggestions && <FollowSuggestion />}
+      {showOptionsDialog && (
+        <OptionsDialog onClose={() => setOptionsDialog(false)} />
+      )}
     </>
   );
 }
