8de65f32367e6a757353d6574ee2d87580496a7c
Lane876 <milenkovicmilan10@gmail.com>
2020-05-28 17:52:58 +0200

    fix

--- src/data.js
+++ src/data.js
@@ -70,7 +70,7 @@ export const defaultCurrentUser = {
   email: "me@gmail.com",
   bio: "This is my bio",
   phone_number: "555-555-5555",
-  posts: Array.from({ length: 10 }, () => getDefaultPost()),
+  posts: [],
   followers: [defaultUser],
   following: [defaultUser],
 };
